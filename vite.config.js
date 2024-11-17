c50e80cd3958cd3436e4281be8fcb7e3c4af64c3
Sathishkumar0404 <sathish@platformatory.com>
2024-11-17 21:46:04 +0530

    updated base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/karnataka-itihasa-academy",
+    base: "/articlesearch",
     define: {
       'process.env':{}
     },
